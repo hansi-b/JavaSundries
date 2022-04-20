1be96bb1ac5d92bbc7390c2409f7efd976042195
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-04-20 21:35:51 +0200

    imported VisibleForTesting

---
+++ sundries/src/main/java/org/hansib/sundries/testing/VisibleForTesting.java
@@ -0,0 +1,44 @@
+/**
+ * MIT License
+ *
+ * for JavaSundries (https://github.com/hansi-b/JavaSundries)
+ *
+ * Copyright (c) 2022 Hans Bering
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.hansib.sundries.testing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks an element as having increased visibility so it can be referenced from
+ * tests.
+ *
+ * Duplicated from countless other VisibleForTesting's so I would not have to
+ * drag in an extra dependency just for this.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface VisibleForTesting {
+	// empty
+}
