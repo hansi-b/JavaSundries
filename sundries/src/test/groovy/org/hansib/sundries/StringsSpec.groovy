acd00b839869c01ecc7439e41f5723383259898f
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2023-01-21 20:51:10 +0100

    usee Java strings where possible

--- sundries/src/test/groovy/org/hansib/sundries/StringsSpec.groovy
+++ sundries/src/test/groovy/org/hansib/sundries/StringsSpec.groovy
@@ -4,26 +4,26 @@ import spock.lang.Specification
 
 public class StringsSpec extends Specification {
 
-	def "get simple id string"() {
+	def 'get simple id string'() {
 		when:
-		def x = "hello"
-		def xHexHash = String.format("%08X", ((Object)x).hashCode())
+		def x = 'hello'
+		def xHexHash = String.format('%08X', ((Object)x).hashCode())
 
 		then:
-		Strings.idStr(x, "world") == "String@${xHexHash}(world)"
+		Strings.idStr(x, 'world') == "String@${xHexHash}(world)"
 	}
 
-	def "join happy paths"() {
+	def 'join happy paths'() {
 		expect:
-		Strings.join("", ['a', 'b', 'c'] as String[], 0, 3) == "abc"
-		Strings.join("_", ['a', 'bb', 'ccc'] as String[], 1, 3) == "bb_ccc"
-		Strings.join("__", ['a', 'bb', 'c'] as String[], 0, 2) == "a__bb"
+		Strings.join('', ['a', 'b', 'c'] as String[], 0, 3) == 'abc'
+		Strings.join('_', ['a', 'bb', 'ccc'] as String[], 1, 3) == 'bb_ccc'
+		Strings.join('__', ['a', 'bb', 'c'] as String[], 0, 2) == 'a__bb'
 	}
 
-	def "join throws ArrayIndexOutOfBoundsException"() {
+	def 'join throws ArrayIndexOutOfBoundsException'() {
 
 		when:
-		Strings.join("", ['a', 'b', 'c'] as String[], 0, 4) == "abc"
+		Strings.join('', ['a', 'b', 'c'] as String[], 0, 4) == 'abc'
 
 		then:
 		thrown ArrayIndexOutOfBoundsException
